e6a8dedacb43ab81f2f61478312f039e12494e4a
AntonJorg <anton2410@gmail.com>
2022-10-04 10:25:22 +0200

    adds multiprocessing for experiments

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -2,13 +2,17 @@ import os
 import argparse
 import logging
 import json
+import random
 from datetime import datetime
+from functools import partial
 from tqdm import tqdm
+from tqdm.contrib.concurrent import process_map
 
 from src.connectfour import ConnectFourState, result
 from src.treesearch import random_agent, mcts_agent, minimax_agent
 
 
+# define command line arguments
 parser = argparse.ArgumentParser(description='Run a number of Connect Four games for \
                                             two agents, saving the results.')
 parser.add_argument('agent0', help="The first agent to make a move.", choices=[
@@ -20,29 +24,31 @@ parser.add_argument(
 parser.add_argument(
     '-t', help="The time available to the agents for taking an action.", type=float, default=1)
 
+# parse command line arguments
 args = parser.parse_args()
 
 
+# set up logging folder structure
 time_string = datetime.now().strftime("%y%m%d-%H%M%S")
-base_filename = os.path.join("logs", time_string)
+folder = os.path.join("logs", time_string)
+os.mkdir(folder)
+os.mkdir(os.path.join(folder, "games"))
+
+print("STARTING EXPERIMENT...")
+print("Configuration:")
+print(args)
+print("Logs will be stored in:", folder)
+print("Progress:")
 
 
 logging.basicConfig(
-    filename=f'{base_filename}.log',
     encoding='utf-8',
     filemode="w",
     level=logging.DEBUG,
     format='%(asctime)s %(levelname)s: %(message)s', datefmt='%d/%m/%Y %H:%M:%S')
 
 
-print("STARTING EXPERIMENT...")
-print("Configuration:")
-print(args)
-print("Logs will be stored in:")
-print(f"{base_filename}.log")
-print(f"{base_filename}.json")
-print("Progress:")
-
+# define function for running one game
 agent_dict = {
     "random": random_agent,
     "mcts": mcts_agent,
@@ -50,23 +56,24 @@ agent_dict = {
 }
 
 
-agents = [agent_dict[args.agent0](args.t), agent_dict[args.agent1](args.t)]
-agent0_wins = 0
-agent1_wins = 0
-draws = 0
-games = []
+def run_game(i, agent0, agent1, t):
+    # make sure that a given game with given agents and time will be identical
+    random.seed(i)
 
-pbar = tqdm(range(args.iterations))
+    # log each game to separate file
+    log_file = os.path.join(folder, "games", f"game_{i}.log")
+    logger = logging.getLogger()
+    logger.handlers = []  # remove all handlers
+    fh = logging.FileHandler(log_file)
+    logger.addHandler(fh)
 
-# TODO: Implement multiprocessing https://www.youtube.com/watch?v=X7vBbelRXn0
+    logger.info(f"STARTING GAME {i}")
 
-for i in pbar:
-
-    logging.info(f"STARTING GAME {i}")
+    agents = [agent_dict[agent0](t), agent_dict[agent1](t)]
 
     state = ConnectFourState(7, 6)
 
-    logging.info(state)
+    logger.info(state)
 
     actions = []
 
@@ -75,30 +82,45 @@ for i in pbar:
         action, root = agents[agent_idx].search(state)
         state = result(state, action)
         actions.append(str(action))
-        logging.info(f"Root: {root}")
-        logging.info("Children:")
+        logger.info(f"Root: {root}")
+        logger.info("Children:")
         for c in root.children:
-            logging.info(c)
-        logging.info(f"Agent {agent_idx} took action {action}")
-        logging.info(state)
+            logger.info(c)
+        logger.info(f"Agent {agent_idx} took action {action}")
+        logger.info(state)
 
-    games.append("".join(actions))
+    game = "".join(actions)
 
-    if state.utility == 1:
+    return i, state.utility, game
+
+
+# insert all the constant arguments
+thread_function = partial(run_game, agent0=args.agent0,
+                          agent1=args.agent1, t=args.t)
+
+# run games on multiple processes
+results = process_map(thread_function, range(args.iterations))
+
+
+# process results
+agent0_wins = 0
+agent1_wins = 0
+draws = 0
+games = {}
+
+for i, utility, game in results:
+    if utility == 1:
         agent0_wins += 1
-    elif state.utility == 0:
+    elif utility == 0:
         agent1_wins += 1
     else:
         draws += 1
 
-logging.info("Results:")
-logging.info(
-    f"Player 1: {agent0_wins / args.iterations * 100:.2f}% ({agent0_wins} wins)")
-logging.info(
-    f"Player 2: {agent1_wins / args.iterations * 100:.2f}% ({agent1_wins} wins)")
-logging.info(f"Draws   : {draws / args.iterations * 100:.2f}% ({draws} games)")
+    games[i] = game
 
-with open(f'{base_filename}.json', 'w') as json_file:
+
+# save results
+with open(os.path.join(folder, "data.json"), 'w') as json_file:
     json.dump(
         {
             "p1_wins": agent0_wins,
